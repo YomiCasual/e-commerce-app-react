5198f4d2fe3e41c10abf68b7bfc92aba5ce4ef41
YomiCasual <yomionisade@gmail.com>
2020-03-24 10:21:50 +0000

    2 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
-import './index.css';
-import App from './App';
+import './src/index.css';
+import App from './src/App';
 import {BrowserRouter as Router} from 'react-router-dom'
-import { Context } from './components/context/context'
+import { Context } from './src/components/context/context'
  
 
 
